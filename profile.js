ac86d0e8329f3f2860742694f582ffe9ae9ef38f
eecanow916 <67128466+eecanow916@users.noreply.github.com>
2023-08-10 13:28:04 -0700

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,8 @@
+// Add event listener to handle connecting with LinkedIn
+document.addEventListener("DOMContentLoaded", function () {
+    const connectLinkedInButton = document.querySelector("#connect-linkedin");
+    connectLinkedInButton.addEventListener("click", function () {
+        // Handle LinkedIn API integration and user authentication here
+        // You'll need to use OAuth 2.0 to authenticate and get user data
+    });
+});
