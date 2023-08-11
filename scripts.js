aed3546bfc62f3346c6dc1b299eba454b8b6fce7
eecanow916 <67128466+eecanow916@users.noreply.github.com>
2023-08-11 14:45:14 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Add event listener to handle navigation to the profile creation page
-document.addEventListener("DOMContentLoaded", function () {
-    const createProfileLink = document.querySelector("#create-profile a");
-    createProfileLink.addEventListener("click", function (event) {
-        event.preventDefault();
-        window.location.href = "create-profile.html"; // Navigate to the profile creation page
-    });
-});
